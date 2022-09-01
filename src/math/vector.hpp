45373e2dc7c47f90e201696cb0a8306ed06438a9
Grubre <ogrodowczykjakub@gmail.com>
2022-09-01 13:49:02 +0200

    feat: Added Vector classes with magnitude functions

--- src/math/vector.hpp
+++ src/math/vector.hpp
@@ -1,5 +1,58 @@
 #pragma once
+#include <type_traits>
+#include <concepts>
+#include <cmath>
 
 namespace dziekan{
-    
+    // Concept encapsulating all numbers (floats, doubles, ints, long longs, etc)
+    template<typename T>
+    concept arithmetic = std::is_arithmetic_v<T>;
+
+
+    template<arithmetic T>
+    struct Base_vec{
+        virtual T mag_sq() = 0;
+        T mag() { return std::sqrt(mag_sq()); };
+    };
+
+
+    template<arithmetic T = double>
+    struct Vec2 : Base_vec<T>
+    {
+        Vec2() : x(0), y(0) {}
+        Vec2(T a, T b) : x(a), y(b) {}
+        T x;
+        T y;
+
+        T mag_sq(){ return x * x + y * y; }
+    };
+
+
+    template<arithmetic T>
+    struct Vec3
+    {
+        Vec3() : x(0), y(0), z(0) {}
+        Vec3(T a, T b, T c) : x(a), y(b), z(c) {}
+
+        T x;
+        T y;
+        T z;
+
+        T mag_sq(){ return x * x + y * y + z * z; }
+    };
+
+
+    template<arithmetic T>
+    struct Vec4
+    {
+        Vec4() : x(0), y(0), z(0), w(0) {}
+        Vec4(T a, T b, T c, T d) : x(a), y(b), z(c), w(d) {}
+
+        T x;
+        T y;
+        T z;
+        T w;
+
+        T mag_sq(){ return x * x + y * y + z * z + w * w; }
+    };
 }
