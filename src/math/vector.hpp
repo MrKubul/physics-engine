dda214c595a3836f70c0a4df53d3be3c770e438f
Grubre <ogrodowczykjakub@gmail.com>
2022-09-01 18:26:12 +0200

    refactor: Made all functions in vector inline

--- src/math/vector.hpp
+++ src/math/vector.hpp
@@ -11,56 +11,56 @@ namespace dziekan{
 
     template<arithmetic T>
     struct Base_vec{
-        const virtual T mag_sq() const = 0;
-        const T mag() const { return std::sqrt(mag_sq()); };
+        inline const virtual T mag_sq() const = 0;
+        inline const T mag() const { return std::sqrt(mag_sq()); };
     };
 
 
     template<arithmetic T = double>
     struct Vec2 : public Base_vec<T>
     {
-        Vec2() : x(0), y(0) {}
-        Vec2(T a, T b) : x(a), y(b) {}
+        inline Vec2() : x(0), y(0) {}
+        inline Vec2(T a, T b) : x(a), y(b) {}
         T x;
         T y;
 
-        const T mag_sq() const { return x * x + y * y; }
-        const T dot(const Vec2 &rhs) const { return x * rhs.x + y * rhs.y; }
-        const Vec2<T> norm() const { return Vec2<T>(x/Vec2<T>::mag(), y/Vec2<T>::mag()); }
-        const Vec2<T> perp() const { return Vec2<T>(-y, x); }
+        inline const T mag_sq() const { return x * x + y * y; }
+        inline const T dot(const Vec2 &rhs) const { return x * rhs.x + y * rhs.y; }
+        inline const Vec2<T> norm() const { return Vec2<T>(x/Vec2<T>::mag(), y/Vec2<T>::mag()); }
+        inline const Vec2<T> perp() const { return Vec2<T>(-y, x); }
     };
 
 
     template<arithmetic T>
     struct Vec3 : public Base_vec<T>
     {
-        Vec3() : x(0), y(0), z(0) {}
-        Vec3(T a, T b, T c) : x(a), y(b), z(c) {}
+        inline Vec3() : x(0), y(0), z(0) {}
+        inline Vec3(T a, T b, T c) : x(a), y(b), z(c) {}
 
         T x;
         T y;
         T z;
 
-        const T mag_sq() const { return x * x + y * y + z * z; }
-        const T dot(const Vec3<T> &rhs) const { return x * rhs.x + y * rhs.y + z * rhs.z; }
-        const Vec3<T> norm() const { return Vec3<T>(x/Vec3<T>::mag(), y/Vec3<T>::mag(), z/Vec3<T>::mag()); }
+        inline const T mag_sq() const { return x * x + y * y + z * z; }
+        inline const T dot(const Vec3<T> &rhs) const { return x * rhs.x + y * rhs.y + z * rhs.z; }
+        inline const Vec3<T> norm() const { return Vec3<T>(x/Vec3<T>::mag(), y/Vec3<T>::mag(), z/Vec3<T>::mag()); }
     };
 
 
     template<arithmetic T>
     struct Vec4 : public Base_vec<T>
     {
-        Vec4() : x(0), y(0), z(0), w(0) {}
-        Vec4(T a, T b, T c, T d) : x(a), y(b), z(c), w(d) {}
+        inline Vec4() : x(0), y(0), z(0), w(0) {}
+        inline Vec4(T a, T b, T c, T d) : x(a), y(b), z(c), w(d) {}
 
         T x;
         T y;
         T z;
         T w;
 
-        const T mag_sq() const { return x * x + y * y + z * z + w * w; }
-        const T dot(const Vec4<T> &rhs) const { return x * rhs.x + y * rhs.y + z * rhs.z + w * rhs.w; }
-        const Vec4<T> norm() const { return Vec4<T>(x/Vec4<T>::mag(), y/Vec4<T>::mag(), z/Vec4<T>::mag(), w/Vec4<T>::mag()); }
+        inline const T mag_sq() const { return x * x + y * y + z * z + w * w; }
+        inline const T dot(const Vec4<T> &rhs) const { return x * rhs.x + y * rhs.y + z * rhs.z + w * rhs.w; }
+        inline const Vec4<T> norm() const { return Vec4<T>(x/Vec4<T>::mag(), y/Vec4<T>::mag(), z/Vec4<T>::mag(), w/Vec4<T>::mag()); }
     };
 
     typedef Vec2<int> Vec2i;
